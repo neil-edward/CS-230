a2e8ad25d9450e7392d6347a46ac4aa2010af83a
evneil <eddie.neil@gmail.com>
2018-11-27 13:41:37 -0500

    Done, I really hope it works on github

--- assignments/weathersite/scripts/prestonweather.js
+++ assignments/weathersite/scripts/prestonweather.js
@@ -15,9 +15,13 @@ request.send();
         else 
             { pi = 0; }
 
+
     
         document.getElementById("currTown").innerText =  jsonObj.list[pi].name;
         document.getElementById("currCon").innerText =  jsonObj.list[pi].weather[0].main;
+        //set icon
+        imgCreate(jsonObj.list[pi].weather[0].icon);
+
         document.getElementById("currTemp").innerHTML =  jsonObj.list[pi].main.temp.toFixed(0);
         document.getElementById("currHum").innerHTML =  jsonObj.list[pi].main.humidity;
         document.getElementById("currWind").innerHTML =  jsonObj.list[pi].wind.speed;
@@ -31,4 +35,13 @@ request.send();
         var s = Math.pow(w, 0.16);
         var f = (35.74 + (0.6215 * t) - (35.75 * s) + (0.4275 * (t * s)));
         return f.toFixed(0);
+    }
+
+    function imgCreate(WIcon) {
+        var src = document.getElementById("currIcon");
+        var img = document.createElement("img");
+        img.src = "http://openweathermap.org/img/w/" + WIcon + ".png";
+        img.alt = "current weather icon"
+        
+        src.appendChild(img);
     }
